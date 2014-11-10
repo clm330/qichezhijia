97bcbaf8772e840bdeec19632e24bee13a029fa5
clm330 <26059847@qq.com>
2014-11-10 09:33:05 +0800

    sql.php

---
+++ sql.php
@@ -0,0 +1,31 @@
+<?php
+
+// 手机，名字，地址，型号，价格
+
+function insert($a,$b,$c,$d,$e){
+
+
+	// echo $a;
+	// echo $b;
+	// echo $c;
+	// echo $d;
+	// echo $e;
+
+	$link = mysql_connect('127.0.0.1', 'root', '123456a');
+	mysql_select_db('qichezhijia', $link);
+
+	echo $a;
+	echo $b;
+	echo $c;
+	echo $d;
+	echo $e;
+
+
+	$sql = "INSERT  INTO `qichezhijia` ( `phone`, `name` , `address` , `type`,`prie`) VALUES ('$a','$b','$c','$d','$e')";
+	//$sql = "INSERT  INTO `qichezhijia` ( `phone`, `name` , `address` , `type`,`prie`) VALUES ('fuck','you','r','mother','kao')";
+	if (!mysql_query($sql,$link))
+	  {
+	  echo 'Error: ' . mysql_error();
+	  }
+
+}
